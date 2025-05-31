1bc034ba37f642a1d34f13132661d34d2301cbc2
sazzadkst27 <shpeasskst@gmail.com>
2025-05-31 23:34:07 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,24 @@
+﻿const cacheName = 'country-game-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('game-cache').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        'manifest.json'
-      ]);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
