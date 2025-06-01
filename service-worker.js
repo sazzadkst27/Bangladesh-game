20eef483e08683d430766c869166fcacc89a37b7
Sazzad Hossain <shpeasskst@gmail.com>
2025-06-01 09:30:38 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,22 @@
-const cacheName = 'country-game-v2';
-const filesToCache = ['/', '/index.html', '/manifest.json'];
+﻿const cacheName = 'animated-country-game-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
